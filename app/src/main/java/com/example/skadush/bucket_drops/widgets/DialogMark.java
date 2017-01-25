5293d3edd8ff687893ca55ea106ce657e45b9825
Nelzkie <nelson.altares@gmail.com>
2017-01-26 01:34:18 +1300

    Recylcer View Item Decoration

---
+++ app/src/main/java/com/example/skadush/bucket_drops/widgets/DialogMark.java
@@ -0,0 +1,55 @@
+package com.example.skadush.bucket_drops.widgets;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import com.example.skadush.bucket_drops.R;
+
+/**
+ * Created by skadush on 25/01/17.
+ */
+public class DialogMark  extends DialogFragment{
+
+    ImageButton mBtnClose;
+    Button mButtonCompleted;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBtnClose = (ImageButton) view.findViewById(R.id.btn_mark_close);
+        mButtonCompleted = (Button) view.findViewById(R.id.btn_mark_completed);
+
+        mButtonCompleted.setOnClickListener(mBtnClickListener);
+        mBtnClose.setOnClickListener(mBtnClickListener);
+
+        Bundle arguments = getArguments();
+        if(arguments != null){
+            int position = arguments.getInt("POSITION");
+            Toast.makeText(getContext(), "" + position, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    View.OnClickListener mBtnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_mark_completed:
+                    break;
+            }
+            dismiss();
+        }
+    };
+}
