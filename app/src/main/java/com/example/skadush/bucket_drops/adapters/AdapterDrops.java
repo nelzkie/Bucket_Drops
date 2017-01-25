f3a6a2ea36de665ec19eaa494bb639c9c4043f6b
Nelzkie <nelson.altares@gmail.com>
2017-01-25 21:03:07 +1300

    Implement Sectioned RecyclerView

--- app/src/main/java/com/example/skadush/bucket_drops/adapters/AdapterDrops.java
+++ app/src/main/java/com/example/skadush/bucket_drops/adapters/AdapterDrops.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import com.example.skadush.bucket_drops.R;
 import com.example.skadush.bucket_drops.beans.Drop;
@@ -16,47 +17,79 @@ import java.util.ArrayList;
 /**
  * Created by skadush on 24/01/17.
  */
-public class AdapterDrops extends RecyclerView.Adapter<AdapterDrops.DropHolder> {
+public class AdapterDrops extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    public static final int ITEM = 0;
+    public static final int FOOTER = 1;
     LayoutInflater mInflater;
     RealmResults<Drop> mResults;
-    public AdapterDrops(Context context,RealmResults<Drop> mResults) {
+
+    public AdapterDrops(Context context, RealmResults<Drop> mResults) {
         mInflater = LayoutInflater.from(context);
         Update(mResults);
 
     }
 
 
-
     @Override
-    public DropHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.row_drop,parent,false);
-        DropHolder holder = new DropHolder(view);
-        return holder;
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == FOOTER) {
+            View view = mInflater.inflate(R.layout.footer, parent, false);
+            return new FooterHolder(view);
+        } else {
+            View view = mInflater.inflate(R.layout.row_drop, parent, false);
+            return new DropHolder(view);
+        }
+
 
     }
 
     @Override
-    public void onBindViewHolder(DropHolder holder, int position) {
-        Drop drop = mResults.get(position);
-        holder.mTextWhat.setText(drop.getWhat());
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof  DropHolder){
+            DropHolder dropHolder = (DropHolder) holder;
+            Drop drop = mResults.get(position);
+            dropHolder.mTextWhat.setText(drop.getWhat());
+        }
+
     }
 
     @Override
     public int getItemCount() {
-        return mResults.size();
+        return mResults.size() + 1;
     }
 
-    public static class DropHolder extends RecyclerView.ViewHolder{
+    public static class DropHolder extends RecyclerView.ViewHolder {
 
         TextView mTextWhat;
+
         public DropHolder(View itemView) {
             super(itemView);
             mTextWhat = (TextView) itemView.findViewById(R.id.tv_what);
         }
     }
+    public static class FooterHolder extends RecyclerView.ViewHolder {
 
-    public void Update(RealmResults<Drop> results){
+        Button mBtnAdd;
+
+        public FooterHolder(View itemView) {
+            super(itemView);
+            mBtnAdd = (Button) itemView.findViewById(R.id.btn_footer);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if (mResults == null || position < mResults.size()) {
+            return ITEM;
+
+        } else {
+            return FOOTER;
+        }
+    }
+
+    public void Update(RealmResults<Drop> results) {
         mResults = results;
         notifyDataSetChanged(); // will refresh the adapter
 
