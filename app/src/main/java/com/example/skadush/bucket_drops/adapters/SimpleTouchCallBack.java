367a019b2901ed4930cfe040a533607681b3a67a
Nelzkie <nelson.altares@gmail.com>
2017-01-27 02:12:16 +1300

    Fix Swipe bug

--- app/src/main/java/com/example/skadush/bucket_drops/adapters/SimpleTouchCallBack.java
+++ app/src/main/java/com/example/skadush/bucket_drops/adapters/SimpleTouchCallBack.java
@@ -1,8 +1,10 @@
 package com.example.skadush.bucket_drops.adapters;
 
+import android.graphics.Canvas;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.support.v7.widget.helper.ItemTouchHelper.Callback;
+import com.example.skadush.bucket_drops.beans.Drop;
 
 /**
  * Created by skadush on 25/01/17.
@@ -33,6 +35,24 @@ public class SimpleTouchCallBack extends Callback {
         return true;
     }
 
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if(viewHolder instanceof AdapterDrops.DropHolder){
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+
+    }
+
+    @Override
+    public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if(viewHolder instanceof AdapterDrops.DropHolder){
+            super.onChildDrawOver(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+
+
+    }
+
     @Override
     public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
         return false;
@@ -40,6 +60,9 @@ public class SimpleTouchCallBack extends Callback {
 
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-        listener.onSwipe(viewHolder.getAdapterPosition());
+        if(viewHolder instanceof AdapterDrops.DropHolder){
+            listener.onSwipe(viewHolder.getAdapterPosition());
+        }
+
     }
 }
