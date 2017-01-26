4dd4c59c411079bdc753240e5f55da956dd42e1d
Nelzkie <nelson.altares@gmail.com>
2017-01-26 21:06:56 +1300

    Update Sorting and added shared pref

---
+++ app/src/main/java/com/example/skadush/bucket_drops/adapters/Filter.java
@@ -0,0 +1,8 @@
+package com.example.skadush.bucket_drops.adapters;
+
+/**
+ * Created by skadush on 26/01/17.
+ */
+public interface Filter {
+    int NONE = 0, MOST_TIME_LEFT = 1, LEAST_TIME_LEFT = 2, COMPETE = 3, INCOMPLETE = 4;
+}
