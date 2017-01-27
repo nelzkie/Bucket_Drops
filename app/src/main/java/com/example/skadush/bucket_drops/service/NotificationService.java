30153c81d068db20f83a927021b2f714d923905a
Nelzkie <nelson.altares@gmail.com>
2017-01-28 01:52:23 +1300

    Added Pug notification

--- app/src/main/java/com/example/skadush/bucket_drops/service/NotificationService.java
+++ app/src/main/java/com/example/skadush/bucket_drops/service/NotificationService.java
@@ -1,11 +1,15 @@
 package com.example.skadush.bucket_drops.service;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
+import br.com.goncalves.pugnotification.notification.PugNotification;
+import com.example.skadush.bucket_drops.ActivityMain;
+import com.example.skadush.bucket_drops.R;
 import com.example.skadush.bucket_drops.beans.Drop;
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -24,8 +28,11 @@ public class NotificationService extends IntentService {
             realm = Realm.getDefaultInstance();
             RealmResults<Drop> results = realm.where(Drop.class).equalTo("completed", false).findAll();
 
+
             for (Drop current : results) {
                 if (isNotificationNeed(current.getAdded(), current.getWhen())) {
+
+                    fireNotification(current);
 
                 }
             }
@@ -34,6 +41,22 @@ public class NotificationService extends IntentService {
                 realm.close();
             }
         }
+    }
+
+    private void fireNotification(Drop drop) {
+        String message = getString(R.string.notif_message) + " \" " + drop.getWhat();
+        PugNotification.with(this)
+                .load()
+                .title(R.string.notif_title)
+                .message(message)
+                .bigTextStyle(R.string.notif_long_message)
+                .smallIcon(R.drawable.pugnotification_ic_launcher)
+                .largeIcon(R.drawable.pugnotification_ic_launcher)
+                .flags(Notification.DEFAULT_ALL)
+                .autoCancel(true)
+                .click(ActivityMain.class)
+                .simple()
+                .build();
     }
 
     private boolean isNotificationNeed(long added, long when) {
